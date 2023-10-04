da968ee61f6c33e6b661cc81bb7234353c8175c2
Nicole Haraj <nicoleharaj@outlook.com>
2023-10-03 21:27:12 -0400

    Remove logo link

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,12 +7,10 @@ import ThemeSwitcher from "./ThemeSelect";
 export default function Navbar() {
   return (
     <nav className="mt-14 flex w-full justify-between">
-      {/* Logo link */}
-      <Link href="/" aria-label="Home" title="Home">
-        <div className="relative h-9 w-8">
-          <Image src="/images/logo.svg" alt="App logo" fill priority />
-        </div>
-      </Link>
+      {/* Logo */}
+      <div className="relative h-9 w-8">
+        <Image src="/images/logo.svg" alt="App logo" fill priority />
+      </div>
 
       <div className="flex items-center gap-5">
         {/* Font selector */}
