bf2a6167aa9b12d765023691be8923620ce6c324
Nicole Haraj <nicoleharaj@outlook.com>
2023-10-04 12:20:12 -0400

    Fix system theme detecting

--- src/app/components/ThemeSelect.tsx
+++ src/app/components/ThemeSelect.tsx
@@ -7,14 +7,31 @@ import { useTheme } from "next-themes";
 export default function ThemeSelect() {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+  const [systemTheme, setSystemTheme] = useState<string | null>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTheme(theme === "dark" ? "light" : "dark");
+    if (systemTheme) {
+      setTheme(systemTheme === "dark" ? "light" : "dark");
+      setSystemTheme(null);
+    } else {
+      setTheme(theme === "dark" ? "light" : "dark");
+    }
   };
 
   useEffect(() => {
     setMounted(true);
   }, []);
+
+  // Detect if using system theme
+  useEffect(() => {
+    if (theme === "system") {
+      const systemPrefersDark =
+        typeof window !== "undefined" &&
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches;
+      setSystemTheme(systemPrefersDark ? "dark" : "light");
+    }
+  }, [theme, systemTheme]);
 
   if (!mounted) {
     return null;
@@ -37,12 +54,16 @@ export default function ThemeSelect() {
           />
           <div
             className={`h-5 w-10 rounded-full ${
-              theme === "dark" ? "bg-purple" : "bg-neutral-400"
+              theme === "dark" || systemTheme === "dark"
+                ? "bg-purple"
+                : "bg-neutral-400"
             } transition-colors group-hover:bg-purple`}
           ></div>
           <div
             className={`absolute left-[3px] top-[3px] flex h-[14px] w-[14px] items-center justify-center rounded-full bg-white transition ${
-              theme === "dark" && "translate-x-[20px]"
+              theme === "dark" || systemTheme === "dark"
+                ? "translate-x-[20px]"
+                : "translate-x-0"
             }`}
           ></div>
         </div>
@@ -50,7 +71,9 @@ export default function ThemeSelect() {
         <div className="h-5 w-5">
           <MoonIcon
             className={`transition-colors ${
-              theme === "dark" ? "stroke-purple" : "stroke-neutral-400"
+              theme === "dark" || systemTheme === "dark"
+                ? "stroke-purple"
+                : "stroke-neutral-400"
             } group-hover:stroke-purple`}
           />
         </div>
